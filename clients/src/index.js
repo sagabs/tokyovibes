e66e758bbd933119fc31bac215624942be5a9d15
Khalil Attalla Firdia <khalilattalla2209@gmail.com>
2022-12-21 16:11:46 +0700

    [GREEN] add carousel in home.js

--- clients/src/index.js
+++ clients/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Home from './pages/Home/index'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
 
@@ -15,3 +16,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
---
+++ clients/src/pages/Home/index.js
@@ -0,0 +1,153 @@
+import Carousel from "react-multi-carousel";
+import { Card, Button, Col, Row } from "react-bootstrap"
+import "./index.css"
+import "react-multi-carousel/lib/styles.css";
+function home() {
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3,
+            slidesToSlide: 2
+        },
+        tablet: {
+            breakpoint: { max: 464, min: 0 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
+
+    return (
+        <div>
+            <h2>Promo</h2>
+            <Carousel responsive={responsive}>
+                <div style={{ margin: 20 }}>
+                    <Card style={{ width: '35rem', height: '15rem', borderRadius: 20, filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))" }}>
+                        <Row>
+                            <Col xs={7}>
+                                <Card.Body style={{ marginTop: 7 }}>
+                                    <Card.Title style={{ textAlign: "center" }}>Card Title</Card.Title>
+                                    <Card.Text style={{ textAlign: "center" }}>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                    </Card.Text>
+                                    <Card.Text style={{ textAlign: "center", textDecoration: "line-through", color: "red" }}>
+                                        <b>Rp. 500.000</b>
+                                    </Card.Text>
+                                    <Card.Text style={{ textAlign: "center" }}>
+                                        <b>Rp. 400.000</b>
+                                    </Card.Text>
+                                </Card.Body>
+
+                            </Col>
+                            <Col style={{ marginTop: 20, marginRight: 10, paddingLeft: 15 }}>
+                                <img src={require("../../assets/img/gambar1.jpg")} height={200} width={200} style={{ borderRadius: 20 }}></img>
+                            </Col>
+                        </Row>
+                    </Card>
+                </div>
+
+                <div style={{ margin: 20 }}>
+                    <Card style={{ width: '35rem', height: '15rem', borderRadius: 20 }}>
+                        <Row>
+                            <Col xs={7}>
+
+                                <Card.Img variant="top" data-src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+
+                            </Col>
+                            <Col style={{ marginTop: 20, marginRight: 10, paddingLeft: 15 }}>
+                                <img src={require("../../assets/img/gambar1.jpg")} height={200} width={200} style={{ borderRadius: 20 }}></img>
+                            </Col>
+                        </Row>
+                    </Card>
+                </div>
+
+                <div style={{ margin: 20 }}>
+                    <Card style={{ width: '35rem', height: '15rem', borderRadius: 20 }}>
+                        <Row>
+                            <Col xs={7}>
+
+                                <Card.Img variant="top" data-src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+
+                            </Col>
+                            <Col style={{ marginTop: 20, marginRight: 10, paddingLeft: 15 }}>
+                                <img src={require("../../assets/img/gambar1.jpg")} height={200} width={200} style={{ borderRadius: 20 }}></img>
+                            </Col>
+                        </Row>
+                    </Card>
+                </div>
+
+                <div style={{ margin: 20 }}>
+                    <Card style={{ width: '35rem', height: '15rem', borderRadius: 20 }}>
+                        <Row>
+                            <Col xs={7}>
+
+                                <Card.Img variant="top" data-src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+
+                            </Col>
+                            <Col style={{ marginTop: 20, marginRight: 10, paddingLeft: 15 }}>
+                                <img src={require("../../assets/img/gambar1.jpg")} height={200} width={200} style={{ borderRadius: 20 }}></img>
+                            </Col>
+                        </Row>
+                    </Card>
+                </div>
+
+                <div style={{ margin: 20 }}>
+                    <Card style={{ width: '35rem', height: '15rem', borderRadius: 20 }}>
+                        <Row>
+                            <Col xs={7}>
+
+                                <Card.Img variant="top" data-src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+
+                            </Col>
+                            <Col style={{ marginTop: 20, marginRight: 10, paddingLeft: 15 }}>
+                                <img src={require("../../assets/img/gambar1.jpg")} height={200} width={200} style={{ borderRadius: 20 }}></img>
+                            </Col>
+                        </Row>
+                    </Card>
+                </div>
+            </Carousel>;
+        </div>
+    )
+}
+
+export default home;
