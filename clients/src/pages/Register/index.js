a452d585e3793f00f4e8ddf4dd29014723c429c1
Afif Fatur Rahman <afiffatur525@gmail.com>
2022-12-21 20:23:29 +0700

    add viewport for regist page

--- clients/src/pages/Register/index.js
+++ clients/src/pages/Register/index.js
@@ -1,10 +1,9 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Bg from '../../assets/bg.png'
-import Logo from '../../assets/logo.png'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Styles from './index.module.css'
-import Form from 'react-bootstrap/Form';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Logo from "../../assets/logo.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Styles from "./index.module.css";
+import Form from "react-bootstrap/Form";
 
 // const myStyle={
 //   backgroundImage:`url(${Bg})`,
@@ -17,13 +16,13 @@ import Form from 'react-bootstrap/Form';
 const Register = () => {
   return (
     <div className={Styles.main}>
-      <div className={Styles.child1}>tes</div>
+      <div className={Styles.child1}></div>
       <div className={Styles.child2}>
         <div className={Styles.regis}>
-          <img className={Styles.logo} src={Logo} alt="logo" />
-          <h4>
-            Daftar
-          </h4>
+          <div className={Styles.logoanddaftar}>
+            <img className={Styles.logo} src={Logo} alt="logo" />
+            <h4>Daftar</h4>
+          </div>
           <Form>
             <Form.Group className={Styles.inputform} required>
               <Form.Label>Nama Depan</Form.Label>
@@ -41,22 +40,28 @@ const Register = () => {
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" placeholder="Masukkan email" />
             </Form.Group>
-            <Form.Group className={Styles.inputform} controlId="formBasicPassword">
+            <Form.Group
+              className={Styles.inputform}
+              controlId="formBasicPassword"
+            >
               <Form.Label>Kata Sandi</Form.Label>
               <Form.Control type="password" placeholder="Enter password" />
             </Form.Group>
-            <Form.Group className={Styles.inputform} controlId="formBasicPassword">
+            <Form.Group
+              className={Styles.inputform}
+              controlId="formBasicPassword"
+            >
               <Form.Label>Konfirmasi Kata Sandi</Form.Label>
               <Form.Control type="password" placeholder="Enter password" />
             </Form.Group>
-            <Button className={Styles.daftar} type="submit">Daftar</Button>
+            <Button className={Styles.daftar} type="submit">
+              Daftar
+            </Button>
           </Form>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
-
-
+export default Register;
