59b8fd6e51db24850cbe89cca24475f6f1809463
fikriaplubis <fikriaplubis@gmail.com>
2022-12-22 10:49:47 +0700

    add axios register

--- clients/src/index.js
+++ clients/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Register from './pages/Register/index';
+// import App from './App';
+//import Register from './pages/Register/index';
 import reportWebVitals from './reportWebVitals';
 import Register from './pages/Register';
 
--- clients/src/pages/Register/index.js
+++ clients/src/pages/Register/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Logo from "../../assets/logo.png";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,8 +6,53 @@ import Styles from "./index.module.css";
 import Form from "react-bootstrap/Form";
 import Gundambg from "../../assets/gundambg.png";
 
+import { API_URL } from "../../utils/constants";
+import axios from "axios";
+import {v4 as uuidv4} from 'uuid';
+
 
 const Register = () => {
+  const [userDetails, setUserDetails] = useState({
+    firstName: "",
+    lastName: "",
+    mobilePhone: "",
+    email: "",
+    password: "",
+    c_password: ""
+  })
+
+  const handleChange = (event) => {
+    setUserDetails({ ...userDetails, [event.target.id]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      id: uuidv4(),
+      firstName: userDetails.firstName,
+      lastName: userDetails.lastName,
+      mobilePhone: userDetails.mobilePhone,
+      email: userDetails.email,
+      password: userDetails.password
+    }
+
+    //console.log(userDetails)
+
+    if (userDetails.password !== userDetails.c_password) {
+      alert("Konfirmasi password belum benar")
+    } else {
+      axios
+        .post(API_URL + "users", data)
+        .then((res) => {
+          alert("Sukses Daftar Akun")
+        })
+        .catch((error) => {
+          console.log("Error yaa", error);
+        });
+    }
+  };
+
   return (
     <div className={Styles.main}>
       <div className={Styles.child1}>
@@ -19,36 +64,64 @@ const Register = () => {
             <img className={Styles.logo} src={Logo} alt="logo" />
             <h4>Daftar</h4>
           </div>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className={Styles.inputform} required>
               <Form.Label>Nama Depan</Form.Label>
-              <Form.Control type="text" placeholder="Masukkan Nama Depan" />
+              <Form.Control 
+                id="firstName"
+                type="text" 
+                placeholder="Masukkan Nama Depan"
+                onChange={handleChange}
+              />
             </Form.Group>
             <Form.Group className={Styles.inputform} required>
               <Form.Label>Nama Belakang</Form.Label>
-              <Form.Control type="text" placeholder="Masukkan Nama Belakang" />
+              <Form.Control
+                id="lastName" 
+                type="text" 
+                placeholder="Masukkan Nama Belakang"
+                onChange={handleChange}
+              />
             </Form.Group>
             <Form.Group className={Styles.inputform} required>
               <Form.Label>Nomor Handphone</Form.Label>
-              <Form.Control type="text" placeholder="Masukkan nomor telepon" />
+              <Form.Control
+                id="mobilePhone" 
+                type="text" 
+                placeholder="Masukkan nomor telepon"
+                onChange={handleChange} 
+              />
             </Form.Group>
-            <Form.Group className={Styles.inputform} controlId="formBasicEmail">
+            <Form.Group className={Styles.inputform}>
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Masukkan email" />
+              <Form.Control 
+                id="email"
+                type="email" 
+                placeholder="Masukkan email"
+                onChange={handleChange} 
+              />
             </Form.Group>
             <Form.Group
               className={Styles.inputform}
-              controlId="formBasicPassword"
             >
               <Form.Label>Kata Sandi</Form.Label>
-              <Form.Control type="password" placeholder="Enter password" />
+              <Form.Control
+                id="password" 
+                type="password" 
+                placeholder="Enter password"
+                onChange={handleChange}
+              />
             </Form.Group>
             <Form.Group
               className={Styles.inputform}
-              controlId="formBasicPassword"
             >
               <Form.Label>Konfirmasi Kata Sandi</Form.Label>
-              <Form.Control type="password" placeholder="Enter password" />
+              <Form.Control
+                id="c_password" 
+                type="password" 
+                placeholder="Enter password"
+                onChange={handleChange}
+              />
             </Form.Group>
             <Button className={Styles.daftar} type="submit">
               Daftar
