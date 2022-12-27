b83f76b6a611af31a791b6d77dd4c94e64703e23
Khalil Attalla Firdia <khalilattalla2209@gmail.com>
2022-12-27 09:46:23 +0700

    change something

--- clients/src/index.js
+++ clients/src/index.js
@@ -19,7 +19,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Tracking />
   </React.StrictMode>
 );
 
